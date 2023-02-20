25b82bd45364c6fc1201aac052e47911af6b0364
Marco Aurelio Garcia Gambacorta <marco.garcia@premmiar.com.br>
2023-02-20 18:27:29 -0300

    remove database connection

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,9 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import List from './List';
+import axios from 'axios';
 
-const handleAddProduct = () => {
+const handleAddProduct = async () => {
     console.log(1111)
+
+    await axios.post("http://localhost:4000/api/product")
+        .then((res) => {
+            console.log(1, res)
+        })
+        .catch((err) => {
+            console.log(2, err)
+        })
 }
 
 const Product = () => {
